4f035459ddacdab81cbb491e25c129a0f6f379fe
Aliaksandr Belavus <aliaksandr_belavus@epam.com>
2022-02-02 12:52:03 +0200

    Home Task for Module 6 Upd 02

--- Protractor/protractor.js
+++ Protractor/protractor.js
@@ -1,41 +1,31 @@
 describe("Protractor Simply Test", function () {
-  it("Simple flight search script", function () {
-    browser.get("https://www.matrix.usag.it/");
-    browser.manage().window().maximize();
 
+  beforeEach(function(){
+    browser.waitForAngularEnabled(false);
+    return browser.manage().window().maximize();
+});
+
+  it("Correct language is displayed", async function () {
+    await browser.get("https://www.matrix.usag.it/");
     //Choosing a language
-    element(by.css('div[class="languageselector ng-scope"]')).click();
-    element(by.css("span[ng-click=\"changeLanguage('en')\"]")).click();
+    await element(by.css('div[class="languageselector ng-scope"]')).click();
+    await element(by.css("span[ng-click=\"changeLanguage('en')\"]")).click();
+    expect(await element(by.css('div[class="languageselector ng-scope"] > span')).getText()).toEqual("EN")
+  });
 
+  it("Possibility to choose the brand and model of the van", async function () {
+    await browser.get("https://www.matrix.usag.it/");
     // Start a project
-    element(by.css('a[href="configurateur.html"]')).click();
-    element(by.css("li[ng-click=\"modeconfiguration='prefab'\"]")).click();
-    element(by.css('li[ng-click="nextStep()"]')).click();
-    browser.manage().setTimeouts({ implicit: 30000 });
+    await element(by.xpath("//span[contains(text(),'start a project')]")).click();
+    await browser.manage().setTimeouts({ implicit: 50000 });
+    await element(by.xpath('//ul[@class="list_radio vehicules"]/li[1]')).click();
+    await element(by.xpath('//li[@title="Next"]')).click();
+    expect(await element(by.xpath('//p[@class="ng-binding ng-scope"]')).getText()).toEqual("2Select brand and model of your van")
+  });
 
-    // Choose brand and model
-    element(by.css('img[ng-src="custom/marques/peugeot.jpg"]')).click();
-    element(
-      by.css(
-        'div[data-prefab="/brands/prefabs/COMPO A1.mtx"][ng-repeat="prefab in prefabs"]'
-      )
-    ).click();
-    element(by.css('li[ng-click="nextStep()"]')).click();
-    browser.manage().setTimeouts({ implicit: 30000 });
-    element(by.css('li[ng-click="nextStep()"]')).click();
-    element(by.css('input[ng-model="barsQuantity"]')).sendKeys(2);
-    element(
-      by.css(
-        'button[class="ngdialog-button ngdialog-button-primary close-dialog ng-binding"]'
-      )
-    ).click();
-
-    //Add the number of accessories
-    element(by.css("input.ng-pristine.ng-untouched.ng-valid")).sendKeys(2);
-    element(by.css("button.ng-binding")).click();
-
-    //Checking the displayed number of accessories
-    expect(element(by.css("div.pied p span.ng-binding")).getText()).toEqual("12");
-    browser.close();
+  it(" How to reach us page is displayed", async function () {
+    await browser.get("https://www.matrix.usag.it/");
+    await element(by.xpath('//a[@href="https://usag.it/en/chisiamo/comeraggiungerci"]')).click();
+    expect(await element(by.xpath('//h1/span')).getText()).toEqual("Where we are")
   });
 });
